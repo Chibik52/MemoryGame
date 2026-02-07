19dd4aadaeb8dc55128fcbcda06e1d67fdb1f7ea
Chibik <kotomkin1337@gmail.com>
2026-02-07 23:00:23 +0300

    fix: desktop card styling and add offline PWA support

--- main.js
+++ main.js
@@ -117,3 +117,10 @@ createApp({
     </main>
   `
 }).mount('#app')
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js').catch(() => {})
+  })
+}
